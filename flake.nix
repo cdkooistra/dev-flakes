3001cb544040fc18fae0a1f2463fbea2a3f06be8
Connor Kooistra <70811244+cdkooistra@users.noreply.github.com>
2025-08-31 20:06:41 +0200

    feat: py versions

--- flake.nix
+++ flake.nix
@@ -4,15 +4,25 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    # each subdir is a flake, specify here
+    python.url = "path:./python";
+    # add more here
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, python, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+
+        py312 = python.devShells.${system}.python312;
+        py313 = python.devShells.${system}.python313;
       in {
         devShells = {
-          python = import ./python/flake.nix { inherit pkgs system; };
+          # python
+          python312 = py312;
+          python313 = py313;
+          python = py313;
         };
       });
 }
--- python/flake.nix
+++ python/flake.nix
@@ -1,31 +1,40 @@
 {
-  description = "Python 3.13 dev. environment";
+  description = "Python dev flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            python313Full
-            uv
-          ];
 
+        py312 = pkgs.mkShell {
+          buildInputs = with pkgs; [ python312Full uv ];
           shellHook = ''
             VENV=.venv
-            if [ ! -d "$VENV" ]; then
-              uv venv $VENV
-            fi
+            if [ ! -d "$VENV" ]; then uv venv $VENV; fi
+            source $VENV/bin/activate
+            echo "Python 3.12 with uv ready ($VENV)"
+          '';
+        };
+
+        py313 = pkgs.mkShell {
+          buildInputs = with pkgs; [ python313Full uv ];
+          shellHook = ''
+            VENV=.venv
+            if [ ! -d "$VENV" ]; then uv venv $VENV; fi
             source $VENV/bin/activate
             echo "Python 3.13 with uv ready ($VENV)"
           '';
         };
-      }
-    );
+      in {
+        devShells = {
+          python312 = py312;
+          python313 = py313;
+          default = py313;
+        };
+      });
 }
