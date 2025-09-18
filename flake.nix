54cd3e1a5214e1da0bc9b386befb2965004d3ca3
Connor Kooistra <70811244+cdkooistra@users.noreply.github.com>
2025-09-18 16:52:08 +0200

    feat: auto process subflakes from inputs

--- flake.nix
+++ flake.nix
@@ -4,31 +4,38 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    
 
     # each subdir is a flake, specify here
     python.url = "path:./python";
     rust.url = "path:./rust";
+    zola.url = "path:./zola";
     # add more here
   };
 
-  outputs = { self, nixpkgs, flake-utils, python, rust, ... }:
+  outputs = inputs@{ nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-
-        py312 = python.devShells.${system}.python312;
-        py313 = python.devShells.${system}.python313;
         
-        rustStable = rust.devShells.${system}.default;
+        # process subflakes
+        subflakes = builtins.removeAttrs inputs [ "nixpkgs" "flake-utils" "self" ];
+        
+        extractShells = name: flake:
+          let shells = flake.devShells.${system} or {};
+          in shells // {
+            # alias the subflake name pointing to default shell
+            ${name} = shells.default or null;
+          };
+        
+        # merge all shells from all subflakes
+        allShells = builtins.foldl' 
+          (acc: name: acc // (extractShells name subflakes.${name}))
+          {}
+          (builtins.attrNames subflakes);
+          
       in {
-        devShells = {
-          # python
-          python312 = py312;
-          python313 = py313;
-          python = py313;
-
-          # rust
-          rust = rustStable;
-        };
+        devShells = builtins.removeAttrs allShells 
+          (builtins.filter (name: allShells.${name} == null) (builtins.attrNames allShells));
       });
 }
